90c8d4b0d913304969c3de24cd373cfe14ea0a74
Herbert <hebertpdl@gmail.com>
2021-02-07 19:22:43 -0300

    creating LoadingSpinner component and adding it to default template

---
+++ components/LoadingSpinner/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="loading-spinner">
+    <img src="../../assets/images/spinner.svg" alt="loading" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingSpinner',
+}
+</script>
+
+<style lang="scss" scoped>
+.loading-spinner {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.8);
+
+  img {
+    width: 60px;
+    height: 60px;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
+    <LoadingSpinner v-if="false" />
     <Nuxt />
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+import LoadingSpinner from '../components/LoadingSpinner'
 
+export default {
+  name: 'Default',
+  components: {
+    LoadingSpinner,
+  },
+}
+</script>
+
+<style>
 * {
   font-family: 'TUITypeLight';
 }
