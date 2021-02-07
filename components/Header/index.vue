5560719d11543199f3c240158f1da882ae5add76
Herbert <hebertpdl@gmail.com>
2021-02-07 14:55:01 -0300

    creating Header component and adding it to default layout

---
+++ components/Header/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <Card :title="$t('header.cardTitle')" />
+    </div>
+  </header>
+</template>
+
+<script>
+import Card from '../Card'
+
+export default {
+  name: 'Header',
+  components: {
+    Card,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 300px;
+  padding: 16px 0;
+  background-color: #1e4785;
+
+  .card {
+    padding: 8px 16px 16px 16px;
+  }
+}
+</style>
