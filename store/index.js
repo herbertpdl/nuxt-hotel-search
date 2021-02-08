6d23ddef446f50357a5fb1127afdf56cea47786a
herbertpdl <hebertpdl@gmail.com>
2021-02-08 15:45:41 -0300

    adding hotel list to store

--- store/index.js
+++ store/index.js
@@ -1,15 +1,22 @@
 export const state = () => ({
   loading: false,
+  hotelsList: [],
 })
 
 export const getters = {
   isLoading: (state) => {
     return state.loading
   },
+  hotelsList: (state) => {
+    return state.hotelsList
+  },
 }
 
 export const mutations = {
   setLoading(state, value) {
     state.loading = value
+  },
+  setHotelsList(state, value) {
+    state.hotelsList = [...value]
   },
 }
